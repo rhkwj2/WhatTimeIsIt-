49fa33c8a60969ec15ebc4a805d751a2e20d06f6
rhkwj <rhkwj@naver.com>
2019-12-09 18:16:44 +0800

    Initial Commit

---
+++ WhatTimeIsIt?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatTimeIsIt?
+//
+//  Created by Kim Yeon Jeong on 2019/12/9.
+//  Copyright © 2019 Kim Yeon Jeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
