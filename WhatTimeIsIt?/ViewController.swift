b4e364af2ff5abb3024d0f8bd5cb240f2a97708e
rhkwj <rhkwj@naver.com>
2019-12-11 15:43:56 +0800

    watch, make mainstay board and main thinking but did not do change good word part

--- WhatTimeIsIt?/ViewController.swift
+++ WhatTimeIsIt?/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var ShowTimeLabel: UILabel!
+    
+    
+    @IBOutlet weak var WordLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //show current time
+        
+        let currentTime = Date()
+        let formetter = DateFormatter()
+        formetter.dateFormat = "MMM/dd E \nHH:mm"
+        let time = formetter.string(from:currentTime)
+        self.ShowTimeLabel.text = time
+        
+       // show Word Label
+        let wordLabelArray=[
+        "Time is money.","Time tries truth.","Time will tell.","Time brings the truth to light.","Time tries friends as fire tries gold.","Time and tide wait for no man.","Time cannot be won again.","Time is a file that wears and makes no noise.","Time stays not the fool's leisure.","Time and I against any two.","Time will tell.","Take time by the forelock."
+        ]
+        //put random
+        let wordLabelRandom = Int.random(in: 1...11)
+        //WordLabel.text = wordLabel
+        //WordLabel.text = String(wordLabel)
+        //WordLabel.text = wordLabelRandom[Int.radom()]
+    
+    
     }
 
 
